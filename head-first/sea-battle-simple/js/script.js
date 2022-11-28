451e63afd05201f66ddd8b8f7bab275dc0813bf2
Alexksxx <bestuzhev.aleksei.m@gmail.com>
2022-11-28 19:43:50 +0300

    update: hf sea-battle cycle done

--- head-first/sea-battle-simple/js/script.js
+++ head-first/sea-battle-simple/js/script.js
@@ -6,3 +6,23 @@ let guess;
 let guesses = 0;
 let hits = 0;
 let isSink = false;
+while (isSink == false) {
+	guess = prompt('Ready, aim, fire! (enter a number 0-6)');
+	if (guess < 0 || guess > 6) {
+		alert('Enter a valid number');
+	}
+	else {
+		guesses = guesses + 1;
+		if (guess == location1 || guess == location2 || guess == location3) {
+			alert('HIT!');
+			hits = hits + 1;
+			if (hits == 3) {
+				isSink = true;
+				alert('You sank my battleship');
+			}
+		}
+		else {
+			alert('MISS');
+		}
+	}
+}
