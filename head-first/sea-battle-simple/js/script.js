8b10a91db4c116de7e456215318a0364e01809c2
Alexksxx <bestuzhev.aleksei.m@gmail.com>
2022-11-28 21:47:48 +0300

    update: hf sea-battle add function

--- head-first/sea-battle-simple/js/script.js
+++ head-first/sea-battle-simple/js/script.js
@@ -1,30 +1,33 @@
 //Написать упрощенную игру "Морской бой" в одну строку от 0 до 6.
-const location1 = 3;
-const location2 = 4;
-const location3 = 5;
-let guess;
-let guesses = 0;
-let hits = 0;
-let isSink = false;
-while (isSink == false) {
-	guess = prompt('Ready, aim, fire! (enter a number 0-6)');
-	if (guess < 0 || guess > 6) {
-		alert('Enter a valid number');
-	}
-	else {
-		guesses = guesses + 1;
-		if (guess == location1 || guess == location2 || guess == location3) {
-			alert('HIT!');
-			hits = hits + 1;
-			if (hits == 3) {
-				isSink = true;
-				alert('You sank my battleship');
-			}
+document.querySelector('.button').onclick = () => {
+	const randomLoc = Math.floor(Math.random() * 5);
+	const location1 = randomLoc;
+	const location2 = location1 + 1;
+	const location3 = location2 + 1;
+	let guess;
+	let guesses = 0;
+	let hits = 0;
+	let isSink = false;
+	while (isSink == false) {
+		guess = prompt('Ready, aim, fire! (enter a number 0-6)');
+		if (guess < 0 || guess > 6) {
+			alert('Enter a valid number');
 		}
 		else {
-			alert('MISS');
+			guesses = guesses + 1;
+			if (guess == location1 || guess == location2 || guess == location3) {
+				alert('HIT!');
+				hits = hits + 1;
+				if (hits == 3) {
+					isSink = true;
+					alert('You sank my battleship');
+				}
+			}
+			else {
+				alert('MISS');
+			}
 		}
 	}
+	const stat = 'You took ' + guesses + ' guesses to sink the battleship, which means your shooting accuransy was ' + (3 / guesses);
+	alert(stat);
 }
-const stat = 'You took ' + guesses + ' guesses to sink the battleship, which means your shooting accuransy was ' + (3 / guesses);
-alert(stat);
