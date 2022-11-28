bcd7e8c1f9deee6ecdb232379dcbab9e0e93002f
Alexksxx <bestuzhev.aleksei.m@gmail.com>
2022-11-28 18:21:16 +0300

    update: hf sea-battle add variables

--- head-first/sea-battle-simple/js/script.js
+++ head-first/sea-battle-simple/js/script.js
@@ -1,1 +1,8 @@
 //Написать упрощенную игру "Морской бой" в одну строку от 0 до 6.
+const location1 = 3;
+const location2 = 4;
+const location3 = 5;
+let guess;
+let guesses = 0;
+let hits = 0;
+let isSink = false;
