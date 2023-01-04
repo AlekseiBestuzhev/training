8582d283bce1619d3bdc6d871a1a8f044e7f166e
Alexksxx <bestuzhev.aleksei.m@gmail.com>
2023-01-05 00:00:13 +0400

    upd: rl gallery hidestreamicon function complete

--- responsive-layout/js/script.js
+++ responsive-layout/js/script.js
@@ -5,6 +5,17 @@ function showMenu() {
 document.querySelector('.burger-button').onclick = showMenu;
 
 function hideStreamIcon() {
-	const streamIcon = document.querySelector('.gallery__stream-icon');
-	streamIcon.style.display = 'none';
+	for (let i = 0; i < streamButtons.length; i++) {
+		const streamIcon = document.querySelector('.gallery__stream-icon');
+		if (streamButtons[0].checked == false) {
+			streamIcon.style.opacity = '1';
+		}
+		else {
+			streamIcon.style.opacity = '0';
+		}
+	}
+}
+const streamButtons = document.querySelectorAll('.gallery__radio');
+for (let i = 0; i < streamButtons.length; i++) {
+	streamButtons[i].onclick = hideStreamIcon;
 }
