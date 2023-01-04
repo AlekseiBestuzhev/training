ded2f425d1c6f0d732876748181417f042867ced
Alexksxx <bestuzhev.aleksei.m@gmail.com>
2023-01-05 01:11:25 +0400

    feat: rl galley add changing of images

--- responsive-layout/js/script.js
+++ responsive-layout/js/script.js
@@ -4,18 +4,29 @@ function showMenu() {
 }
 document.querySelector('.burger-button').onclick = showMenu;
 
-function hideStreamIcon() {
+function changeContent() {
 	for (let i = 0; i < streamButtons.length; i++) {
 		const streamIcon = document.querySelector('.gallery__stream-icon');
-		if (streamButtons[0].checked == false) {
-			streamIcon.style.opacity = '1';
-		}
-		else {
+		const imageShow = document.querySelector('.gallery__img');
+		if (streamButtons[0].checked) {
 			streamIcon.style.opacity = '0';
+			imageShow.setAttribute('src', "img/gallery/gallery-image-1.jpg");
+		}
+		else if (streamButtons[1].checked) {
+			streamIcon.style.opacity = '1';
+			imageShow.setAttribute('src', "img/gallery/gallery-image-2.jpg");
+		}
+		else if (streamButtons[2].checked) {
+			streamIcon.style.opacity = '1';
+			imageShow.setAttribute('src', "img/gallery/gallery-image-3.jpg");
+		}
+		else if (streamButtons[3].checked) {
+			streamIcon.style.opacity = '1';
+			imageShow.setAttribute('src', "img/gallery/gallery-image-4.jpg");
 		}
 	}
 }
 const streamButtons = document.querySelectorAll('.gallery__radio');
 for (let i = 0; i < streamButtons.length; i++) {
-	streamButtons[i].onclick = hideStreamIcon;
+	streamButtons[i].onclick = changeContent;
 }
